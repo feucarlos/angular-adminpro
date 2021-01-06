7c83a8810f8eb415352d2272535d69a20f7471e4
José Carlos Gutiérez Rojas <feucalos@gmail.com>
2021-01-05 21:35:36 -0600

    noPageFound

--- src/app/pages/nopagefound/nopagefound.component.ts
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
-  styles: [
-  ]
+  styleUrls: [ './nopagefound.component.css' ]
 })
-export class NopagefoundComponent implements OnInit {
+export class NopagefoundComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+year = new Date().getFullYear();
 }
