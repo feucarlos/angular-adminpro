1e8b66f2e60d2fc4a58d578161dd2d3973f6688f
José Carlos Gutiérez Rojas <feucalos@gmail.com>
2021-01-11 20:43:58 -0600

    servicio para cambio de tema

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -7,21 +8,18 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AccountSettingsComponent implements OnInit {
-  
-  public linkTheme = document.querySelector('#theme');
 
-  constructor() { }
+  constructor(private settingsService: SettingsService) { }
 
   ngOnInit(): void {
+    this.settingsService.checkCurrentTheme();
   }
 
   changeTheme(theme: string) {
-    
-    const url = `./assets/css/colors/${ theme }.css`;
 
-    this.linkTheme.setAttribute('href', url);
-    localStorage.setItem('theme', url);
-    
+    this.settingsService.changeTheme( theme );
   }
+
+  
 
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
 
 @Component({
   selector: 'app-pages',
@@ -8,15 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
-  public linkTheme = document.querySelector('#theme');
+  
 
-  constructor() { }
+  constructor(private settingsService: SettingsService) { }
 
-  ngOnInit(): void {
-
-    const url = localStorage.getItem('theme') || './assets/css/colors/default-dark.css';
-    this.linkTheme.setAttribute('href', url);
-
-  }
+  ngOnInit(): void {}
 
 }
