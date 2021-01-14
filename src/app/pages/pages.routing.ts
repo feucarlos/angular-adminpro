e0f184ba3ac113b139b4ee560e1b2bd4c0665c8c
José Carlos Gutiérez Rojas <feucalos@gmail.com>
2021-01-13 21:11:55 -0600

    Titulos de secciones y de documento

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -15,12 +15,12 @@ const routes: Routes = [
         path: 'dashboard',
         component: PagesComponent,
         children: [
-            { path: '', component: DashboardComponent },
-            { path: 'acount-settings', component: AccountSettingsComponent },
-            { path: 'grafica1', component: Grafica1Component },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'promesas', component: PromesasComponent },
-            { path: 'rxjs', component: RxjsComponent }
+            { path: '', component: DashboardComponent, data: {titulo: 'Dashboard'} },
+            { path: 'acount-settings', component: AccountSettingsComponent, data: {titulo: 'Configuración de tema'}},
+            { path: 'grafica1', component: Grafica1Component, data: {titulo: 'Gráfica #1'} },
+            { path: 'progress', component: ProgressComponent, data: {titulo: 'ProgressBar'} },
+            { path: 'promesas', component: PromesasComponent, data: {titulo: 'Promesas'} },
+            { path: 'rxjs', component: RxjsComponent, data: {titulo: 'Rxjs'} }
             // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
         ]
     },
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
 
-  constructor() { }
+  titulo: string;
+  tituloSubs$: Subscription;
 
-  ngOnInit(): void {
+  constructor(private router: Router) { 
+    this.tituloSubs$ =  this.getTituloRuta().subscribe( ({ titulo }) => {
+                        this.titulo = titulo;
+                        document.title = 'Adminpro: ' + titulo;
+                        });
   }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getTituloRuta(){
+    return  this.router.events
+    .pipe(
+      filter( event => event instanceof ActivationEnd ),
+      filter( (event: ActivationEnd ) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd ) => event.snapshot.data  )
+    )
+    // .subscribe( ({ titulo }) => {
+    //   this.titulo = titulo;
+    //   document.title = 'Adminpro: ' + titulo;
+    // } )
+
+  }
+
 
 }
