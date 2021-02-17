3cb099266a395d1a43385efd37323b7f85f79b2f
José Carlos Gutiérez Rojas <feucalos@gmail.com>
2021-02-16 22:41:58 -0600

    autenticar con google

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario.service';
+
 import Swal from 'sweetalert2';
+
+declare const gapi: any;
 
 
 @Component({
@@ -13,10 +16,11 @@ import Swal from 'sweetalert2';
 export class LoginComponent implements OnInit {
 
   public formSubmitted = false;
-
+  public auth2: any;
+  
   public loginForm = this.fb.group({
-    email: ['test16@elbraus.com', [Validators.required, Validators.email] ],
-    password: ['123456', Validators.required ],
+    email: [localStorage.getItem('email') || '', [Validators.required, Validators.email] ],
+    password: ['', Validators.required ],
     remember: [false]
   });
 
@@ -25,16 +29,75 @@ export class LoginComponent implements OnInit {
               private usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
+    this.renderButton();
   }
 
   login(){
     // this.router.navigateByUrl('/');
     this.usuarioService.login(this.loginForm.value)
       .subscribe( resp => {
-        console.log(resp);
+        if (this.loginForm.get('remember').value){
+          localStorage.setItem('email', this.loginForm.get('email').value);
+        } else {
+          localStorage.removeItem('email');
+        }
       }, (err)=>{
         Swal.fire('Error', err.error.msg, 'error'); 
       });
     
   }
+
+//   onSuccess(googleUser) {
+//     console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+//     const id_token = googleUser.getAuthResponse().id_token;
+//     // console.log(id_token);
+//  }
+
+//   onFailure(error) {
+//     console.log(error);
+//   }
+
+  renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      // 'onsuccess': this.onSuccess,
+      // 'onfailure': this.onFailure
+    });
+    this.startApp();
+  }
+
+  startApp(){
+    gapi.load('auth2', ()=> {
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      this.auth2 = gapi.auth2.init({
+        client_id: '626070206867-sqlp8fuq0dff5tn4hn6kuo0r3vri67u4.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      this.attachSignin(document.getElementById('my-signin2'));
+    });
+  };
+
+  attachSignin(element) {
+    // console.log(element.id);
+    this.auth2.attachClickHandler(element, {},
+        (googleUser) => {
+          // document.getElementById('name').innerText = "Signed in: " +
+          //     googleUser.getBasicProfile().getName();
+          const id_token = googleUser.getAuthResponse().id_token;
+          // console.log(id_token);
+          this.usuarioService.loginGoogle(id_token).subscribe();
+          // TODO: redireccionar el dashboard
+          
+        }, function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        });
+  }
+
+
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+
+
 import { environment } from 'src/environments/environment';
+
+
 import { LoginForm } from '../interfaces/login-form.interface';
 import { RegisterForm } from '../interfaces/register-form.interface';
 
@@ -13,14 +18,33 @@ export class UsuarioService {
 
   constructor(private http: HttpClient) { }
 
-  crearUsuario( formData: RegisterForm ){
-    
-    return this.http.post(`${base_url}/usuarios`, formData );
-    
+  crearUsuario(formData: RegisterForm) {
+
+    return this.http.post(`${base_url}/usuarios`, formData)
+      .pipe(
+        tap((resp: any) => {
+          localStorage.setItem('token', resp.token)
+        })
+      );
+
   }
 
-  login(formData: LoginForm){
-    return this.http.post(`${base_url}/login`, formData );
+  login(formData: LoginForm) {
+    return this.http.post(`${base_url}/login`, formData)
+      .pipe(
+        tap((resp: any) => {
+          localStorage.setItem('token', resp.token)
+        })
+      );
+  }
+
+  loginGoogle(token) {
+    return this.http.post(`${base_url}/login/google`, {token})
+      .pipe(
+        tap((resp: any) => {
+          localStorage.setItem('token', resp.token)
+        })
+      );
   }
 
 }
